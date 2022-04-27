ddaa38157c619334f30746056aaeea6eb0312312
Bren Briggs <code@fraq.io>
2022-04-27 11:57:48 -0400

    safhjklwserthjkqawertdfg

---
+++ docker/install-cuda.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+apt-get update
+apt-get install -y --no-install-recommends \
+        build-essential \
+        curl \
+        libfreetype6-dev \
+        libhdf5-serial-dev \
+        libzmq3-dev \
+        pkg-config \
+        software-properties-common \
+        unzip \
+        cuda-command-line-tools-${CUDA/./-} \
+        libcublas-${CUDA/./-} \
+        cuda-nvrtc-${CUDA/./-} \
+        libcufft-${CUDA/./-} \
+        libcurand-${CUDA/./-} \
+        libcusolver-${CUDA/./-} \
+        libcusparse-${CUDA/./-}
+#        libcudnn8-${CUDNN}+cuda${CUDA}
