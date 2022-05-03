abb83a04d5a91dbbb89883a5e5e6c0c2dae13d9d
Rodolfo <rodolfo.sekijima@hypergiant.com>
2022-05-03 23:08:47 +0900

    Read ProjectName from manifest

--- hyper/client/manifest/yaml.go
+++ hyper/client/manifest/yaml.go
@@ -1,14 +1,19 @@
 package manifest
 
 import (
-	"gopkg.in/yaml.v2"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Manifest struct {
 	StudyName   string `yaml:"study_name"`
 	ModelFlavor string `yaml:"model_flavor"`
+	ProjectName string `yaml:"project_name"`
 	Training    struct {
 		Data struct {
 			Features struct {
@@ -32,9 +37,29 @@ func GetManifest(manifestPath string) Manifest {
 		log.Fatalf("Unmarshal: %v", err)
 	}
 
+	if m.ProjectName == "" {
+		cwdPath, _ := os.Getwd()
+		cwdName := strings.Replace(cwdPath, "/", "-", -1)
+		m.ProjectName = fmt.Sprintf("firefly-jupyter-%s", cwdName)
+
+		str := string(yamlFile)
+
+		updatedYAML := fmt.Sprintf("project_name: %s\n%s", m.ProjectName, str)
+		err := ioutil.WriteFile(manifestPath, []byte(updatedYAML), 0)
+
+		if err != nil {
+			log.Printf("yamlFile.Write err   #%v ", err)
+		}
+
+	}
+
 	return m
 }
 
 func GetName(manifestPath string) string {
 	return GetManifest(manifestPath).StudyName
+}
+
+func GetProjectName(manifestPath string) string {
+	return GetManifest(manifestPath).ProjectName
 }
