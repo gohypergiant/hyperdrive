21d4ae82277c315af15803ee57e998144edd5937
John Hou <john.hou@hypergiant.com>
2022-05-04 09:17:03 -0700

    pull out images not being built

--- hyper/services/notebook/utils.go
+++ hyper/services/notebook/utils.go
@@ -27,41 +27,11 @@ func GetNotebookImageOptions(flavor string) ImageOptions {
 
 	// TODO: We could just make this a map, and even read it in from a remote endpoint
 	switch flavor {
-	case "huggingface-pytorch":
-		return ImageOptions{
-			Image:   "ghcr.io/gohypergiant/hyperdrive-jupyter:cpu-huggingface-pytorchstable",
-			RepoTag: "hyperdrive-jupyter-cpu-huggingface-pytorch",
-			Profile: "hugging-face-pytorch-cpu",
-		}
-	case "huggingface-tensorflow":
-		return ImageOptions{
-			Image:   "ghcr.io/gohypergiant/hyperdrive-jupyter:cpu-huggingface-tensorflowstable",
-			RepoTag: "hyperdrive-jupyter-cpu-huggingface-tensorflow",
-			Profile: "hugging-face-tensorflow-cpu",
-		}
 	case "pytorch":
 		return ImageOptions{
 			Image:   "ghcr.io/gohypergiant/hyperdrive-jupyter:cpu-pytorchstable",
 			RepoTag: "hyperdrive-jupyter-cpu-pytorch",
 			Profile: "pytorch-cpu",
-		}
-	case "spark":
-		return ImageOptions{
-			Image:   "ghcr.io/gohypergiant/hyperdrive-jupyter:cpu-sparkstable",
-			RepoTag: "hyperdrive-jupyter-cpu-spark",
-			Profile: "spark-cpu",
-		}
-	case "tensorflow":
-		return ImageOptions{
-			Image:   "ghcr.io/gohypergiant/hyperdrive-jupyter:cpu-tensorflowstable",
-			RepoTag: "hyperdrive-jupyter-cpu-tensorflow",
-			Profile: "tensorflow-cpu",
-		}
-	case "xgboost":
-		return ImageOptions{
-			Image:   "ghcr.io/gohypergiant/hyperdrive-jupyter:cpu-xgbooststable",
-			RepoTag: "hyperdrive-jupyter-cpu-xgboost",
-			Profile: "xgboost-cpu",
 		}
 	case "dev":
 		return ImageOptions{
