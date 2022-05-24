a1cdc7852c9743da522f98a4d10ed146439e44dc
John Hou <john.hou@hypergiant.com>
2022-05-24 13:09:58 +0900

    change image name

--- hyper/services/notebook/local.go
+++ hyper/services/notebook/local.go
@@ -77,7 +77,7 @@ func (s LocalNotebookService) Start(flavor string, pullImage bool, jupyterBrowse
 
 	if requirements {
 		dockerClient.CreateDockerFile("", "Dockerfile.reqs", true)
-		dockerClient.BuildImage("Dockerfile.reqs", []string{"reqs-test:latest"})
+		dockerClient.BuildImage("Dockerfile.reqs", []string{"hyperdrive-jupyter-reqs:latest"})
 	}
 
 	if len(runningContainers) == 0 {
