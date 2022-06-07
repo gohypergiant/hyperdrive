9a8ebe389af4898218b3991596273d516d0429af
John Hou <john.hou@hypergiant.com>
2022-06-07 20:20:07 +0900

    import_model filename arg

--- hypertrain/hypertrain/controllers/import_model.py
+++ hypertrain/hypertrain/controllers/import_model.py
@@ -1,9 +1,9 @@
 from hypertrain.flavor.sklearn import SklearnModelHandler
 
 
-def import_trained_model(flavor="dingo"):
+def import_trained_model(filename="", flavor="sklearn"):
     if flavor == "sklearn":
-        sklearn_model = SklearnModelHandler._load_model("finalized_model.sav")
+        sklearn_model = SklearnModelHandler._load_model(filename)
         shape = 8
         onnx_model = SklearnModelHandler._convert(sklearn_model, shape)
         SklearnModelHandler._save(onnx_model, "trained_model")
