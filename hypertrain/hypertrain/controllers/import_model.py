ed5739b760f4938b54e4532778fd9892bd82da6e
John Hou <john.hou@hypergiant.com>
2022-06-07 22:05:35 +0900

    zip the model

--- hypertrain/hypertrain/controllers/import_model.py
+++ hypertrain/hypertrain/controllers/import_model.py
@@ -1,4 +1,5 @@
 from hypertrain.flavor.sklearn import SklearnModelHandler
+from hypertrain.utilities import zip_study
 
 
 def import_trained_model(filename="", flavor="sklearn", shape=0):
@@ -7,3 +8,4 @@ def import_trained_model(filename="", flavor="sklearn", shape=0):
         onnx_model = SklearnModelHandler._convert(sklearn_model, shape)
         save_filename = "imported_" + flavor + "_model"
         SklearnModelHandler._save(onnx_model, save_filename)
+        zip_study(save_filename)
