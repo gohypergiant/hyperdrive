db12935bb27b50bba5320fba7b402eda7d7f1d17
John Hou <john.hou@hypergiant.com>
2022-06-29 20:53:45 +0900

    flesh out func to generate api key

--- hyper/client/cli/key.go
+++ hyper/client/cli/key.go
@@ -1,7 +1,20 @@
 package cli
 
+import (
+    "fmt"
+    "os"
+    
+    "github.com/sethvargo/go-password/password"
+)
+
 func getFastAppAPIKey() string {
-    fastAppApiKey := ""
-	
+    fastAppApiKey, err := password.Generate(64, 10, 0, true, true)
+    if err != nil {
+        fmt.Println(err)
+		os.Exit(1)
+    }
+    
+    log.Printf("A FastApp Token of %s has been generated.", fastAppApiKey)
+
 	return fastAppApiKey
 }
