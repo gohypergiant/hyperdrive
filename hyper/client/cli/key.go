0d9ae356c64c1de9fe2a19e7f163dcc833b53c47
John Hou <john.hou@hypergiant.com>
2022-06-29 21:32:03 +0900

    key should be all lowercase

--- hyper/client/cli/key.go
+++ hyper/client/cli/key.go
@@ -8,13 +8,11 @@ import (
 )
 
 func generateFastAppAPIKey() string {
-    fastAppApiKey, err := password.Generate(64, 10, 0, false, true)
+    fastAppApiKey, err := password.Generate(64, 10, 0, true, true)
     if err != nil {
         fmt.Println(err)
 		os.Exit(1)
     }
-    
-    fmt.Println("Here is the generated FastApp API key:", fastAppApiKey)
 
 	return fastAppApiKey
 }
