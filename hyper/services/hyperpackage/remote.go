9481f467b0a51f38848011abb9728c9857417e26
Rodolfo <rodolfo.sekijima@hypergiant.com>
2022-06-30 22:11:47 +0900

    Reorder functions to match local

--- hyper/services/hyperpackage/remote.go
+++ hyper/services/hyperpackage/remote.go
@@ -1,13 +1,16 @@
 package hyperpackage
 
+import "github.com/gohypergiant/hyperdrive/hyper/types"
+
 type RemoteHyperpackageService struct {
-	HyperpackagePath string
-	ManifestPath     string
+	HyperpackagePath    string
+	ManifestPath        string
+	RemoteConfiguration types.ComputeRemoteConfiguration
 }
 
+func (s RemoteHyperpackageService) BuildAndRun(dockerfileSavePath string, imageTags []string) {}
 func (s RemoteHyperpackageService) Build(dockerfileSavePath string, imageTags []string)       {}
 func (s RemoteHyperpackageService) Run(imageTag string)                                       {}
-func (s RemoteHyperpackageService) BuildAndRun(dockerfileSavePath string, imageTags []string) {}
 func (s RemoteHyperpackageService) Import(importModelFileName string, modelFlavor string, trainShape string) {
 }
 func (s RemoteHyperpackageService) List()            {}
