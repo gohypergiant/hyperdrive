9e46fd54bfe6e38c6788724e19e4079140a97e25
Rodolfo <rodolfo.sekijima@hypergiant.com>
2022-07-07 23:06:01 +0900

    Add WriteFileToEC2 method

--- hyper/client/aws/ec2.go
+++ hyper/client/aws/ec2.go
@@ -5,13 +5,14 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
-	config2 "github.com/gohypergiant/hyperdrive/hyper/services/config"
 	"io/fs"
 	"os"
 	"path"
 	"reflect"
 	"runtime"
 	"time"
+
+	config2 "github.com/gohypergiant/hyperdrive/hyper/services/config"
 
 	"github.com/docker/distribution/uuid"
 	hyperdriveTypes "github.com/gohypergiant/hyperdrive/hyper/types"
@@ -920,5 +921,23 @@ func StopServer(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemoteC
 			panic("error deleting VPC," + err.Error())
 		}
 		fmt.Println("VPC deleted:", vpcID)
+	}
+}
+
+func WriteFileToEC2(instanceIp string, remoteCfg hyperdriveTypes.EC2ComputeRemoteConfiguration, projectName string, filePath string) {
+
+	keyName := projectName
+	sshFolderPath := path.Join(UserHomeDir(), "/.ssh")
+	privateKeyPath := path.Join(sshFolderPath, fmt.Sprintf("/%s", keyName))
+
+	err := ssh.CopyToRemote("ec2-user", privateKeyPath, instanceIp, filePath, "./")
+	if err != nil {
+		privateKeyPath = path.Join(sshFolderPath, fmt.Sprintf("/%s", ssh.DEFAULT_KEY))
+		err = ssh.CopyToRemote("ec2-user", privateKeyPath, instanceIp, filePath, "./")
+		if err != nil {
+			fmt.Println("Cannot copy file to EC2 server")
+			os.Exit(1)
+			return
+		}
 	}
 }
