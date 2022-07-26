a1e2392d06d6d0b23275111bbac4e950e7274c1c
John Hou <john.hou@hypergiant.com>
2022-07-26 21:11:24 +0900

    2 func args w/ error handling

--- hyperpackage/hyperpackage/hyperpack_creation.py
+++ hyperpackage/hyperpackage/hyperpack_creation.py
@@ -1,2 +1,6 @@
-def create_hyperpack():
+def create_hyperpack(trained_model=None, model_flavor: str = None):
+    if trained_model is None:
+        raise ValueError("You must pass in a trained model.")
+    elif model_flavor is None:
+        raise ValueError("You must specify a model flavor.")
     print("ahoy environs!")
