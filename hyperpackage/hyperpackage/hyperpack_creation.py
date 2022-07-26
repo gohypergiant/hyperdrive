5d728429175c7a39876183e586f1dd49331ef88f
John Hou <john.hou@hypergiant.com>
2022-07-26 21:20:11 +0900

    denote SUPPORTED_MODEL_FLAVORS

--- hyperpackage/hyperpackage/hyperpack_creation.py
+++ hyperpackage/hyperpackage/hyperpack_creation.py
@@ -1,6 +1,11 @@
+SUPPORTED_MODEL_FLAVORS = ["automl"]
+
+
 def create_hyperpack(trained_model=None, model_flavor: str = None):
     if trained_model is None:
         raise ValueError("You must pass in a trained model.")
     elif model_flavor is None:
-        raise ValueError("You must specify a model flavor.")
+        print("You must specify a model flavor. Supported model flavors are:\n")
+        print("\n".join(map(str, SUPPORTED_MODEL_FLAVORS)))
+        raise ValueError("Please specify a model flavor.")
     print("ahoy environs!")
