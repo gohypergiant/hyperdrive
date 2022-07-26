5a0a62ee6a1076ed2d8df6def01cb0e8f1b11efa
John Hou <john.hou@hypergiant.com>
2022-07-26 21:34:18 +0900

    cleanup err msg, ValueError s/b TypeError

--- hyperpackage/hyperpackage/hyperpack_creation.py
+++ hyperpackage/hyperpackage/hyperpack_creation.py
@@ -3,9 +3,12 @@ SUPPORTED_MODEL_FLAVORS = ["automl"]
 
 def create_hyperpack(trained_model=None, model_flavor: str = None):
     if trained_model is None:
-        raise ValueError("You must pass in a trained model.")
+        raise TypeError("You must pass in a trained model.")
     elif model_flavor is None:
-        print("You must specify a model flavor. Supported model flavors are:\n")
-        print("\n".join(map(str, SUPPORTED_MODEL_FLAVORS)))
-        raise ValueError("Please specify a model flavor.")
+        supported_flavors = "\n".join(map(str, SUPPORTED_MODEL_FLAVORS))
+        raise TypeError(
+            "You must specify a model flavor. Supported model flavors are:\n{}".format(
+                supported_flavors
+            )
+        )
     print("ahoy environs!")
