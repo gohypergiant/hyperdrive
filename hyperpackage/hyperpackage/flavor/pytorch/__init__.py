bf27d41eb02d9faf9112f2b124da4a7692a9c19d
John Hou <john.hou@hypergiant.com>
2022-07-27 21:43:27 +0900

    verify_args func, create hyperpack_path

--- hyperpackage/hyperpackage/flavor/pytorch/__init__.py
+++ hyperpackage/hyperpackage/flavor/pytorch/__init__.py
@@ -1,10 +1,10 @@
+import os
 import torch
 
 
 def torch_onnx_export(
-    trained_model,
-    local_artifact_path="/home/jovyan/automl_trained_model",
-    train_shape=30,
+    model, hyperpack_dir: str, train_shape: int = 30,
 ):
+    local_artifact_path = os.path.join(hyperpack_dir, "trained_model")
     initial_types = torch.randn(1, train_shape)
-    torch.onnx.export(trained_model, initial_types, local_artifact_path, train_shape)
+    torch.onnx.export(model, initial_types, local_artifact_path, train_shape)
--- hyperpackage/hyperpackage/hyperpack_creation.py
+++ hyperpackage/hyperpackage/hyperpack_creation.py
@@ -1,17 +1,39 @@
+import os
+from pathlib import Path
 from hyperpackage.flavor.pytorch import torch_onnx_export
 
 SUPPORTED_MODEL_FLAVORS = ["automl"]
 
 
 def create_hyperpack(trained_model=None, model_flavor: str = None):
-    if trained_model is None:
+    verify_args(model=trained_model, flavor=model_flavor)
+    hyperpack_path = make_hyperpack_path(name=model_flavor)
+    try:
+        os.makedirs(hyperpack_path, exist_ok=False)
+    except FileExistsError:
+        i = 1
+        while os.path.exists(f"{hyperpack_path}_{str(i)}"):
+            i += 1
+        hyperpack_path = hyperpack_path + "_" + str(i)
+        os.makedirs(hyperpack_path, exist_ok=False)
+    torch_onnx_export(model=trained_model, hyperpack_dir=hyperpack_path)
+    print("ahoy environs!")
+
+
+def verify_args(model, flavor: str):
+    if model is None:
         raise TypeError("You must pass in a trained model.")
-    elif model_flavor is None:
+    elif flavor is None:
         supported_flavors = "\n".join(map(str, SUPPORTED_MODEL_FLAVORS))
         raise TypeError(
             "You must specify a model flavor. Supported model flavors are:\n{}".format(
                 supported_flavors
             )
         )
-    torch_onnx_export(trained_model=trained_model)
-    print("ahoy environs!")
+
+
+def make_hyperpack_path(name: str) -> str:
+    home_dir = str(Path.home())
+    hyperpack_folder_name = name + ".hyperpack"
+    path = os.path.join(home_dir, hyperpack_folder_name)
+    return path
