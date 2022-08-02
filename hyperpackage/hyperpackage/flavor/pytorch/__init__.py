53cf9ae16fdc3467b52f56b2af41741e952bdf9f
John Hou <john.hou@hypergiant.com>
2022-08-02 21:45:52 +0900

    var name change

--- hyperpackage/hyperpackage/flavor/pytorch/__init__.py
+++ hyperpackage/hyperpackage/flavor/pytorch/__init__.py
@@ -5,6 +5,6 @@ import torch
 def torch_onnx_export(
     model, trial_path: str, train_shape_cols: int,
 ):
-    local_artifact_path = os.path.join(trial_path, "trained_model")
+    model_path = os.path.join(trial_path, "trained_model")
     initial_types = torch.randn(1, train_shape_cols)
-    torch.onnx.export(model, initial_types, local_artifact_path, train_shape_cols)
+    torch.onnx.export(model, initial_types, model_path, train_shape_cols)
