51703979db8d26ce353a0356ccb3ced7237fad2b
Craig Midwinter <craig.j.midwinter@gmail.com>
2022-09-07 15:45:52 -0500

    fix some other small errors

--- hyper/client/aws/ec2.go
+++ hyper/client/aws/ec2.go
@@ -327,18 +327,7 @@ func setSubnetToProvisionPublicIP(subnetID string, client *ec2.Client) {
 }
 func getOrCreateSubnet(client *ec2.Client, vID string, region string, projectName string, rtID string) string {
 
-	subnetDescribeInput := &ec2.DescribeSubnetsInput{
-		Filters: []types.Filter{
-			{
-				Name:   aws.String("vpc-id"),
-				Values: []string{vID},
-			},
-		},
-	}
-	subnetDescribeResult, err := GetSubnets(context.TODO(), client, subnetDescribeInput)
-	if err != nil {
-		panic("error fetching Subnets," + err.Error())
-	}
+	subnetDescribeResult, err := describeSubnets(client, vID)
 
 	subnetID := GetSubnetID(subnetDescribeResult, projectName)
 
@@ -375,6 +364,22 @@ func getOrCreateSubnet(client *ec2.Client, vID string, region string, projectNam
 	}
 
 	return subnetID
+}
+
+func describeSubnets(client *ec2.Client, vID string) (*ec2.DescribeSubnetsOutput, error) {
+	subnetDescribeInput := &ec2.DescribeSubnetsInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String("vpc-id"),
+				Values: []string{vID},
+			},
+		},
+	}
+	subnetDescribeResult, err := GetSubnets(context.TODO(), client, subnetDescribeInput)
+	if err != nil {
+		panic("error fetching Subnets," + err.Error())
+	}
+	return subnetDescribeResult, err
 }
 
 func getOrCreateSecurityGroup(client *ec2.Client, vID string, projectName string, httpPort int) string {
@@ -580,14 +585,42 @@ func GetInstanceForStudy(studyName string, remoteCfg hyperdriveTypes.EC2ComputeR
 func IsStructureEmpty(i types.Instance) bool {
 	return reflect.DeepEqual(i, types.Instance{})
 }
+
 func StartJupyterEC2(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemoteConfiguration, ec2Type string, amiID string, jupyterLaunchOptions hyperdriveTypes.JupyterLaunchOptions, syncOptions hyperdriveTypes.WorkspaceSyncOptions) {
+	if isJupyterInstanceRunning(manifestPath, remoteCfg) {
+		return
+	}
+
 	startupScript := getJupyterEc2StartScript(version, jupyterLaunchOptions, syncOptions, remoteCfg)
+
 	ip := StartServer(manifestPath, remoteCfg, ec2Type, amiID, startupScript, jupyterLaunchOptions.HostPort)
 
 	if ip != "" {
 		fmt.Println("In a few minutes, you should be able to access jupyter lab at http://" + ip + ":8888/lab")
 
 	}
+}
+
+func isJupyterInstanceRunning(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemoteConfiguration) bool {
+	projectName := manifest.GetProjectName(manifestPath)
+	hyperInstance, err := GetInstanceForStudy(projectName, remoteCfg)
+	if err != nil {
+		fmt.Println(err)
+		return true
+	}
+
+	if !IsStructureEmpty(hyperInstance) {
+		message := fmt.Sprintf(`Hyper instance already running.
+Instance size: %s
+You can access jupyter lab at http://%s:8888/lab
+If you want to change the instance size, stop the current running instance:
+
+	hyper jupyter stop --remote=<REMOTE_PROFILE_NAME>
+`, hyperInstance.InstanceType, *hyperInstance.PublicIpAddress)
+		fmt.Println(message)
+		return true
+	}
+	return false
 }
 func StartHyperpackageEC2(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemoteConfiguration, ec2Type string, amiID string, syncOptions hyperdriveTypes.WorkspaceSyncOptions, dockerOptions hyperdriveTypes.DockerOptions) {
 	startupScript := getHyperpackageEC2StartScript(version, dockerOptions, syncOptions, remoteCfg)
@@ -623,24 +656,6 @@ func StartServer(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemote
 	fmt.Println("Project name is:", projectName)
 	client := GetEC2Client(remoteCfg)
 
-	hyperInstance, err := GetInstanceForStudy(projectName, remoteCfg)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-
-	if !IsStructureEmpty(hyperInstance) {
-		message := fmt.Sprintf(`Hyper instance already running.
-Instance size: %s
-You can access jupyter lab at http://%s:8888/lab
-If you want to change the instance size, stop the current running instance:
-
-	hyper jupyter stop --remote=<REMOTE_PROFILE_NAME>
-`, hyperInstance.InstanceType, *hyperInstance.PublicIpAddress)
-		fmt.Println(message)
-		return ""
-	}
-
 	vpcID, rtID := getOrCreateVPC(client, projectName)
 	fmt.Println("VPC ID:", vpcID)
 	fmt.Println("Route Table ID:", rtID)
@@ -656,6 +671,7 @@ If you want to change the instance size, stop the current running instance:
 
 	minMaxCount := int32(1)
 	ec2Input := &ec2.RunInstancesInput{
+
 		ImageId:           aws.String(amiID),
 		InstanceType:      types.InstanceType(*aws.String(ec2Type)),
 		MinCount:          aws.Int32(minMaxCount),
@@ -755,7 +771,7 @@ func getHyperpackageEC2StartScript(version string, dockerOptions hyperdriveTypes
 		hostPort = dockerOptions.HostPort
 	}
 
-	syncParameters := fmt.Sprintf("--s3AccessKey %s --s3Secret %s --s3Token %s --s3Region %s --s3BucketName %s -n %s", syncOptions.S3Config.AccessKey, syncOptions.S3Config.Secret, syncOptions.S3Config.Token, syncOptions.S3Config.Region, syncOptions.S3Config.BucketName, syncOptions.StudyName)
+	syncParameters := fmt.Sprintf("--workspaceS3AccessKey %s --sworkspaceS3Secret %s --workspaceS3Token %s --workspaceS3Region %s --workspaceS3BucketName %s -n %s", syncOptions.S3Config.AccessKey, syncOptions.S3Config.Secret, syncOptions.S3Config.Token, syncOptions.S3Config.Region, syncOptions.S3Config.BucketName, syncOptions.StudyName)
 	runParameters := fmt.Sprintf("--hyperpackagePath %s.hyperpack.zip --hostPort %d --localOnly=false %s", syncOptions.StudyName, hostPort, syncParameters)
 	startupScript := fmt.Sprintf(`
 #!/bin/bash -xe
@@ -977,7 +993,7 @@ func StopServer(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemoteC
 				}
 				_, err = DisassociateRouteTable(context.TODO(), client, routeTableDisassociateInput)
 				if err != nil {
-					panic("error disassociating Route Table," + err.Error())
+					println("error disassociating Route Table," + err.Error())
 				}
 			}
 			routeTableDeleteInput := &ec2.DeleteRouteTableInput{
@@ -985,7 +1001,7 @@ func StopServer(manifestPath string, remoteCfg hyperdriveTypes.EC2ComputeRemoteC
 			}
 			_, err = DeleteRouteTable(context.TODO(), client, routeTableDeleteInput)
 			if err != nil {
-				panic("error deleting Route Table," + err.Error())
+				println("error deleting Route Table," + err.Error())
 			}
 			fmt.Println("Route Table deleted:", routeTableID)
 		}
