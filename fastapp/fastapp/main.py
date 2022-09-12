7ad3f75b72e9b843f4e28bc66fd38dc16fc6566d
John Arnold <john@johnarnold.ca>
2022-09-12 11:12:41 -0500

    Implement UI to upload a hyperpack

--- fastapp/fastapp/main.py
+++ fastapp/fastapp/main.py
@@ -2,7 +2,8 @@ import logging
 import os
 from logging.config import dictConfig
 
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from fastapp.logging.config import LogConfig
 from fastapp.routers import model
@@ -15,9 +16,11 @@ logger.error("Dummy Error")
 logger.debug("Dummy Debug")
 logger.warning("Dummy Warning")
 
-app = FastAPI(title="mlsdk-fastapp")
-app.include_router(model.router)
+api_app = FastAPI(title="mlsdk-fastapp-api")
+api_app.include_router(model.router)
 
+app = FastAPI(title="mlsdk-fastapp-web")
+app.mount("/api", api_app)
 
 @app.on_event("startup")
 def show_fast_app_api_key():
@@ -28,13 +31,8 @@ def show_fast_app_api_key():
     fast_key_msg = "Fast App API key is: {api_key}".format(api_key=fast_key)
     logger.info(fast_key_msg)
 
-
-api_router = APIRouter()
-
-
-@api_router.get("/status", status_code=200)
+@app.get("/status", status_code=200)
 def root() -> dict:
     return {"status": "ok"}
 
-
-app.include_router(api_router)
+app.mount("/", StaticFiles(directory="fastapp/ui", html=True), name="ui")
