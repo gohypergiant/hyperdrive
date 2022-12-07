e06e14a794ba4e4710f95d6ff2ebdf91c05203d6
John Hou <john.hou@hypergiant.com>
2022-12-07 07:54:10 -0800

    edit xgboost docstring

--- hyperpackage/hyperpackage/flavor/xgboost/__init__.py
+++ hyperpackage/hyperpackage/flavor/xgboost/__init__.py
@@ -12,7 +12,6 @@ def xgboost_onnx_save(model, trial_path: str, train_shape_cols: int):
         trial_path: dir path to the trial
         train_shape_cols: the number of columns in the training dataset used to
                           train the pretrained model
-        ml_task : the type of machine learning task
     """
     model_path = os.path.join(trial_path, "trained_model")
     initial_types = [("X", FloatTensorType([None, train_shape_cols]))]
