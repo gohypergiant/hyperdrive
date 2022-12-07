b8c770fd61eae30538972dc468c105936f545dab
John Hou <john.hou@hypergiant.com>
2022-12-07 09:03:14 -0800

    might as well fix tensorflow docstring

--- hyperpackage/hyperpackage/flavor/tensorflow/__init__.py
+++ hyperpackage/hyperpackage/flavor/tensorflow/__init__.py
@@ -4,6 +4,14 @@ import tf2onnx
 
 
 def tensorflow_onnx_export(model, trial_path: str):
+    """Saves tensorflow model to ONNX format
+    Args:
+        model: pretrained model
+        trial_path: dir path to the trial
+        train_shape_cols: the number of columns in the training dataset used to
+                          train the pretrained model
+        ml_task : the type of machine learning task
+    """
     onnx_model, _ = tf2onnx.convert.from_keras(model)
     model_path = os.path.join(trial_path, "trained_model")
     onnx.save(onnx_model, model_path)
