9774f63939247613b62147c0fef8ba3d40b93fca
John Hou <john.hou@hypergiant.com>
2022-12-06 16:21:36 -0800

    flesh out xgboost_onnx_save func

--- hyperpackage/hyperpackage/flavor/xgboost/__init__.py
+++ hyperpackage/hyperpackage/flavor/xgboost/__init__.py
@@ -6,4 +6,8 @@ from skl2onnx.common.data_types import FloatTensorType
 
 
 def xgboost_onnx_save(model, trial_path: str, train_shape_cols: int):
-    print("xgboost to onnx")
+    model_path = os.path.join(trial_path, "trained_model")
+    initial_types = [("X", FloatTensorType([None, train_shape_cols]))]
+    onnx_model = onnxmltools.convert.convert_xgboost(
+        model, initial_types=initial_types)
+    onnx.save(onnx_model, model_path)
