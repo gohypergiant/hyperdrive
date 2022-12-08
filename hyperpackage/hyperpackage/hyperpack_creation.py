ca5c91bd9fb3610d171eb7bc9f5db7dfab6bc382
John Hou <john.hou@hypergiant.com>
2022-12-08 14:48:28 -0800

    create_study_json docstring edit

--- hyperpackage/hyperpackage/hyperpack_creation.py
+++ hyperpackage/hyperpackage/hyperpack_creation.py
@@ -264,7 +264,7 @@ def create_study_json(hyperpack_path: str, best_trial: str, ml_task: str, flavor
         best_trial: name of best trial
         ml_task: type of machine learning task
         flavor: library/package used to build pretrained model.
-                Examples include automl, pytorch, and tensorflow
+                Examples include automl, pytorch, tensorflow, and xgboost
     """
     created_time = datetime.now().strftime("%Y-%m-%d %H:%M")
     study_json_dict = {
